2d9516bd1f9c41b72e38131bfeb3827a43fcf55a
Anna <tutivanna2@gmail.com>
2020-07-23 22:22:55 +0300

    WIP Added user profile changing

--- view/profile.php
+++ view/profile.php
@@ -5,31 +5,132 @@ $user = new Object('user');
 $user_data = $user->getById($_SESSION['id']);
 $name = $user_data['name'];
 $img = $user_data['avatar'];
-$login = ($_SESSION['login']);
+$login = $user_data['login'];
 $about = $user_data['about'];
-if(!$img){
-    $img = "/images/corgi.jpg";
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['login'])) {
+        $newlogin = $_POST['login'];
+        if ($login == '') {
+            unset($login);
+        }
+    }
+    if (isset($_POST['about'])) {
+        $newabout = $_POST['about'];
+        if ($newabout == '') {
+            unset($newabout);
+        }
+    }
+    if (isset($_POST['name'])) {
+        $newname = $_POST['name'];
+        if ($newname == '') {
+            unset($newname);
+        }
+    }
+    if (isset($_POST['userfile'])) {
+        $uploaddir = $_SERVER['DOCUMENT_ROOT']."/user_images/";
+        $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+        if (!file_exists($uploaddir)){
+            mkdir($uploaddir, 0700);
+        }
+        if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+            echo ("<script> alert('Файл корректен и был успешно загружен')</script>");
+        } else {
+            echo ("<script> alert('Возможная атака с помощью файловой загрузки!')</script>");
+        }
+    }
+
+    $conn = Connection::getInstance();
+
+    $newlogin = $conn->escape_string($newlogin);
+    $newlogin = htmlspecialchars($newlogin);
+    $newname = $conn->escape_string($newname);
+    $newname = htmlspecialchars($newname);
+    $newabout = htmlspecialchars($newabout);
+
+    $newlogin = trim($newlogin);
+    $newabout = trim($newabout);
+    $newname = trim($newname);
+
+    if ($about != $newabout) {
+        $array = array(
+            "about" => $newabout,
+        );
+        $user->updateById($array, $_SESSION['id']);
+        $about = $newabout;
+
+    }
+    if ($name != $newname) {
+        $array = array(
+            "name" => $newname,
+        );
+        $user->updateById($array, $_SESSION['id']);
+        $name = $newname;
+    }
+
+    if ($login != $newlogin) {
+
+        $query = sprintf("SELECT id FROM user WHERE login='$newlogin'");
+
+        $myrow = $conn->query($query)->fetch_all(MYSQLI_ASSOC);
+
+        if (count($myrow) != 0) {
+            h_die("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
+        }
+        $array = array(
+            "login" => $newlogin,
+        );
+        $user->updateById($array, $_SESSION['id']);
+        $login = $newlogin;
+        $_SESSION['login']=$myrow['login'];
+    }
 }
-if(!$about){
-    $about = "Расскажите о себе!";
+function h_die($err) {
+    echo '<h5 class="text-center">' . $err . '</h5>';
+    exit();
 }
 ?>
+<script>
+    const a = document.getElementById("edit_profile");
+    a.style.display = "None";
+    function hideEditProfile() {
+        a.style.display = "";
+    }
+</script>
 
-<form class="form-profile" method="post">
-    <img src=<? echo ($img) ?> width="10%" align="left" border="0" hspace="2%" vspace="2%" />
+<div class="mx-auto text form-profile" style="max-width: 800px;">
+
+    <img src=
+         <? if(!$img){
+            echo "/images/corgi.jpg";
+         }else {
+             echo $img;
+         }?>
+         width="200px" align="left" border="0" hspace="2%" vspace="2%" />
     <h4 class="font-weight-normal">
         <?echo("Имя: ".$name)?>
     </h4>
     <h4 class="font-weight-normal"><?echo ("Логин: " . $login)?></h4>
     <h4 class="font-weight-normal">Обо мне:</h4>
-    <p><?echo ($about)?></p>
+    <p><? if(!$about){
+            echo "Расскажите о себе!";
+        }else {
+            echo $about;
+        }?></p>
+    <input type="button" class="btn btn-lg btn-primary btn-block" onclick="hideEditProfile()" value="Редактировать">
+</div>
 
-<!--    <h1 class="font-weight-normal">Авторизация</h1>-->
-<!--    <label for="inputLogin" class="sr-only">Логин</label>-->
-<!--    <input name="login" type="text" class="form-control" id="inputLogin" placeholder="Логин" required autofocus>-->
-<!--    <label for="inputPassword" class="sr-only">Пароль</label>-->
-<!--    <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Пароль" required>-->
-<!--    <button class="btn btn-lg btn-primary btn-block" type="submit">Вход</button>-->
+<div class="mx-auto text" style="max-width: 600px; border: solid black 1px; margin: 20px">
+<form enctype="multipart/form-data" class="mx-auto text form-profile" id="edit_profile" method="post">
+    <h5 class="font-weight-normal">Введите новые данные</h5>
+    <h5> Логин </h5>
+    <input name="login" type="text" id="inputLogin" class="form-control" value="<?echo ($login)?>">
+    <h5> Имя </h5>
+    <input name="name" type="text" id="inputName" class="form-control" value="<?echo ($name)?>">
+    <h5> О себе </h5>
+    <textarea name="about" class="form-control" id="inputAbout"><?echo ($about)?></textarea>
+    <h5> Аватар </h5>
+    <input name="userfile" type="file" />
+    <button class="btn btn-lg btn-primary mx-auto" type="submit">Принять</button>
 </form>
 
 
